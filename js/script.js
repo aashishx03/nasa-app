da1a51ebbc3dc7a0dc2bdeb1700491a56055c417
Aashish <aashishdubey620@gmail.com>
2023-07-18 19:06:05 +0530

    added design

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 "use strict";
 
-fetch(
-  "https://api.nasa.gov/planetary/apod?api_key=3osSgW195PY2g6E4sKrj4jsMycfLlsWrxLAgHvD4"
-)
-  .then((res) => res.json())
-  .then((data) => {
-    document.querySelector(
-      ".container"
-    ).style.backgroundImage = `url(${data.hdurl})`;
-  });
+// fetch(
+//   "https://api.nasa.gov/planetary/apod?api_key=3osSgW195PY2g6E4sKrj4jsMycfLlsWrxLAgHvD4"
+// )
+//   .then((res) => res.json())
+//   .then((data) => {
+//     document.querySelector(
+//       ".container"
+//     ).style.backgroundImage = `url(${data.hdurl})`;
+//   });
 
 document.querySelector(".btn").addEventListener("click", showData);
 
@@ -16,16 +16,17 @@ function showData() {
   let val = document.querySelector("input").value;
   // console.log(val);
   // console.log("working");
+
   fetch(
     `https://api.nasa.gov/planetary/apod?date=${val}&api_key=3osSgW195PY2g6E4sKrj4jsMycfLlsWrxLAgHvD4`
   )
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-
+      document.querySelector("img").classList.remove("hidden");
       document.querySelector("img").src = data.hdurl;
       document.querySelector("h1").textContent = data.title;
-      document.querySelector("h4").innerHTML = data.explanation;
+      document.querySelector("#expln").innerHTML = data.explanation;
     })
     .catch((err) => console.log(err));
 }
